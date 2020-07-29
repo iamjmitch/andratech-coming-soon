974a193af182c8bc23e7c449051dbf0bd506cc8d
James Mitchell <iamjmitch1990@gmail.com>
2020-07-29 23:16:22 +1000

    add google analytics code

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,3 +50,19 @@ if (window.innerWidth > 1500) {
     }
   }, 50);
 }
+
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'G-NP7Z8X94EC']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src =
+    ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+    '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
